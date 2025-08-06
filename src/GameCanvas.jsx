0e84afa16dc0b1c7c4374b04a5c4cd23c4a7b046
JinKota-NIXE <85142319+JinKota-NIXE@users.noreply.github.com>
2025-08-06 18:10:51 +0900

    Update GameCanvas.jsx

--- src/GameCanvas.jsx
+++ src/GameCanvas.jsx
@@ -32,7 +32,8 @@ export default function GameCanvas() {
 
     /* === 状態 === */
     let px = W / 2 - 60, py = H - 120;
-    const bullets = [], enemies = [], boss = [];
+    const bullets = [], enemies = [];
+    let boss = [];
     let score = 0, frame = 0, enemiesCount = 0, bossHP = 10;
 
     /* ライフとゲームオーバーフラグ */
