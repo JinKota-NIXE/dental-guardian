c292a5b2ab2253939dabdda684e44fdddccd20ba
JinKota-NIXE <85142319+JinKota-NIXE@users.noreply.github.com>
2025-08-07 17:47:52 +0900

    Update GameCanvas.jsx

--- src/GameCanvas.jsx
+++ src/GameCanvas.jsx
@@ -70,7 +70,7 @@ export default function GameCanvas() {
         enemies.forEach((e) => (e.y += 2));
 
         // ボス出現（スコアが20越えた時＆50ごとに、まだ出現していない場合）
-        if (score !== 0 && score <= 20 && score % 50 === 0 && !bossRef.current) {
+        if (score !== 0 && score = 20 && score % 50 === 0 && !bossRef.current) {
           console.log("true2");
           boss = { x: W / 2 - 100, y: -200 }; // 初期位置は画面外上
           bossRef.current = true;
